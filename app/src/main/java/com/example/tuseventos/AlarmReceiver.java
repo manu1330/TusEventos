ffb54ab8299d636d43581a4eee7c80f76d3a7c79
manu1330 <manu28082001@gmail.com>
2022-06-05 13:38:26 +0200

    Cambio

--- app/src/main/java/com/example/tuseventos/AlarmReceiver.java
+++ app/src/main/java/com/example/tuseventos/AlarmReceiver.java
@@ -5,11 +5,17 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.util.concurrent.ExecutionException;
 
@@ -21,19 +27,25 @@ public class AlarmReceiver extends BroadcastReceiver {
         String titulo = intent.getStringExtra("titulo");
         String id = intent.getStringExtra("id");
         String imagen = intent.getStringExtra("imagen");
-        System.out.println("Alarme disparado");
+        System.out.println("Alarma");
         createNotificationChannel(context);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "1");
         builder.setSmallIcon(R.drawable.logo_periodico);
         builder.setContentTitle(titulo);
         builder.setContentText("Ha llegado la hora de su evento");
-        try {
-            builder.setLargeIcon(Glide.with(context).asBitmap().skipMemoryCache(true).load(imagen).into(150, 150).get());
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        Glide.with(context)
+//                .asBitmap()
+//                .load(Tags.SERVER + imagen.substring(1))
+//                .into(new CustomTarget<Bitmap>() {
+//                    @Override
+//                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+//                        builder.setLargeIcon(resource);
+//                    }
+//
+//                    @Override
+//                    public void onLoadCleared(@Nullable Drawable placeholder) {
+//                    }
+//                });
         builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -45,7 +57,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "canal 1";
-            String description = "descrição do canal 1";
+            String description = "Descripcion del canal 1";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel("1", name, importance);
             channel.setDescription(description);
